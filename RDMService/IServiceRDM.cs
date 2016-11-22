fcc66a133bbe4be31a8eef9a7a91b764f4db4200
jef <jean-francois.vaissettes@2isa.org>
2016-11-22 16:10:28 +0100

    Remote Desktop Monitor

---
+++ RDMService/IServiceRDM.cs
@@ -0,0 +1,35 @@
+﻿using ConsumeWebServiceRest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RDMService
+{
+    [ServiceContract]
+    public interface IServiceRDM
+    {
+        [OperationContract]
+        [WebInvoke(UriTemplate = "Login", Method = "POST", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+        WSR_Result Login(WSR_Params p);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "Logout", Method = "POST", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+        WSR_Result Logout(WSR_Params p);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "GetPseudos", Method = "POST", RequestFormat = WebMessageFormat.Xml, ResponseFormat = WebMessageFormat.Xml, BodyStyle = WebMessageBodyStyle.Bare)]
+        WSR_Result GetPseudos(WSR_Params p);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "UploadData", Method = "POST", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+        WSR_Result UploadData(WSR_Params p);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "DownloadData", Method = "POST", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+        WSR_Result DownloadData(WSR_Params p);
+    }
+}
---
+++ RDMService/ServiceRDM.svc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using ConsumeWebServiceRest;
+
+namespace RDMService
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" dans le code, le fichier svc et le fichier de configuration.
+    // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez Service1.svc ou Service1.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+    public class ServiceRDM : IServiceRDM
+    {
+        public WSR_Result DownloadData(WSR_Params p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public WSR_Result GetPseudos(WSR_Params p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public WSR_Result Login(WSR_Params p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public WSR_Result Logout(WSR_Params p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public WSR_Result UploadData(WSR_Params p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
