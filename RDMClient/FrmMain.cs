422cfc046a12f475e7eff093d45dc148d97f6233
jef <jean-francois.vaissettes@2isa.org>
2016-12-01 10:08:25 +0100

    Afficher dans la liste

--- RDMClient/FrmMain.cs
+++ RDMClient/FrmMain.cs
@@ -17,10 +17,17 @@ namespace RDMClient
         internal const string ADR_SERVICE_DEBUG = "http://localhost:60078/";
 
         private RdmDalWSR _rdmDal = new RdmDalWSR();
+
         public FrmMain()
         {
             InitializeComponent();
+
+            // On fixe l'adresse de base du service Web
+            //_rdmDal.StringConnect = txtbWebService.Text;
+
+            // On fixe l'adresse de base du service Web en mode Debug (exécution du service en local)
             _rdmDal.StringConnect = ADR_SERVICE_DEBUG;
+            txtWebService.Text = ADR_SERVICE_DEBUG;
         }
 
         #region "Texte Changed"
@@ -33,55 +40,80 @@ namespace RDMClient
         private void txtPseudo_TextChanged(object sender, EventArgs e)
         {
             _rdmDal.PseudoConnect = txtPseudo.Text;
-            btConnecter.Enabled = (!String.IsNullOrWhiteSpace(txtPseudo.Text));
+            btConnect.Enabled = (!String.IsNullOrWhiteSpace(txtPseudo.Text));
         }
 
         private void txtPassword_TextChanged(object sender, EventArgs e)
         {
 
-            btDeconnecter.Enabled = (!String.IsNullOrWhiteSpace(txtPassword.Text));
+            btDeconnect.Enabled = (!String.IsNullOrWhiteSpace(txtPassword.Text));
         }
         #endregion
 
-        #region "Evenement click"
+        #region "Evenements Panel Connexion"
+
         private async void btConnecter_Click(object sender, EventArgs e)
         {
-            pnlConnexion.Enabled = false;
-            RdmDalWSRResult ret = await _rdmDal.LoginAsync(CancellationToken.None);
+            btConnect.Enabled = true;
+            btDeconnect.Enabled = false;
+            _rdmDal.PseudoConnect = txtPseudo.Text;
+            RdmDalWSRResult ret1 = await _rdmDal.LoginAsync(CancellationToken.None);
 
-            if (ret.IsSuccess)
+            if (ret1.IsSuccess)
             {
                 txtWebService.Enabled = false;
-                txtPseudo.Enabled = false;
-                txtPassword.Text = (string)ret.Data;
+                txtPseudo.Enabled = true;
+                txtPassword.Text = (string)ret1.Data;
                 lblErreur.Text = "Vous êtes connecté";
             }
             else
             {
-                lblErreur.Text = ret.ErrorMessage;
+                lblErreur.Text = ret1.ErrorMessage;
             }
 
-            pnlConnexion.Enabled = true;
+            RdmDalWSRResult ret2 = await _rdmDal.GetPseudosAsync(CancellationToken.None);
+
+            if (ret2.IsSuccess && _rdmDal.IsLogged)
+            {
+                AfficheListePseudos(ret2);
+            }
+            else
+            {
+                lblErreur.Text = ret2.ErrorMessage;
+            }
+            btConnect.Enabled = false;
+            btDeconnect.Enabled = true;
         }
 
         private async void btDeconnecter_Click(object sender, EventArgs e)
         {
-            pnlConnexion.Enabled = false;
-            RdmDalWSRResult ret = await _rdmDal.LogoutAsync(CancellationToken.None);
+            btConnect.Enabled = false;
+            btDeconnect.Enabled = false;
 
-            if (ret.IsSuccess)
+            RdmDalWSRResult ret1 = await _rdmDal.LogoutAsync(CancellationToken.None);
+
+
+            if (ret1.IsSuccess)
             {
+
                 lblErreur.Text = "Vous n'êtes pas connecté";
             }
             else
             {
-                lblErreur.Text = "Vous n'êtes pas connecté" + ret.ErrorMessage;
+                lblErreur.Text = "Vous n'êtes pas connecté" + ret1.ErrorMessage;
             }
-            pnlConnexion.Enabled = true;
+
+            btConnect.Enabled = false;
+            btDeconnect.Enabled = true;
+
+            // On ré-initialise les champs de la fenêtre
             lstbPseudos.Items.Clear();
             txtWebService.Enabled = true;
             txtPseudo.Enabled = true;
             txtPassword.Text = String.Empty;
+            txtPseudo.Text = String.Empty;
+            btConnect.Enabled = true;
+            btDeconnect.Enabled = false;
         }
 
         private void btAnnuler_Click(object sender, EventArgs e)
@@ -89,6 +121,31 @@ namespace RDMClient
             Close();
         }
 
+        /// <summary>
+        /// Fonction permettant d'afficer les pseudos dans la liste
+        /// </summary>
+        /// <param name="result"></param>
+        private void AfficheListePseudos(RdmDalWSRResult result)
+        {
+            List<String> lstResult = (List<string>)result.Data;
+
+            //Suppression des pseudos déconnectés
+            for (int i = lstbPseudos.Items.Count - 1; i >= 0; i--)
+            {
+                if (!lstResult.Contains(lstbPseudos.Items[i]))
+                {
+                    lstbPseudos.Items.RemoveAt(i);
+                }
+            }
+            //Ajout des news pseudos
+            foreach (string pseudo in lstResult)
+            {
+                if (!lstbPseudos.Items.Contains(pseudo))
+                {
+                    lstbPseudos.Items.Add(pseudo);
+                }
+            }
+        }
         #endregion
     }
 }
